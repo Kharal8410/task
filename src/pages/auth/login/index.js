f2c7d02824356ba4c1cabb86d1079a9f7be7cb33
mukunda kharal <kharalmukunda2@gmail.com>
2024-02-04 19:04:48 +0545

    fett update

--- src/pages/auth/login/index.js
+++ src/pages/auth/login/index.js
@@ -1,73 +1,100 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import toast, { Toaster } from "react-hot-toast";
 import AuthContext from "../../../context/AuthContext";
 
 const Login = () => {
   const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
+
   const initialValue = {
     name: "",
     password: "",
   };
-  const [formValue, setFormValue] = useState(initialValue);
-  const [formError, setFormError] = useState({});
-  const [isSubmit, setIsSubmit] = useState(false);
-
-  const navigate = useNavigate();
+  const [formValues, setFormValues] = useState(initialValue);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormValue({ ...formValue, [name]: value });
+    setFormValues({ ...formValues, [name]: value });
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const errors = validate(formValues);
+    setFormErrors(errors);
+    setIsSubmitting(true);
+  };
+
   const validate = (values) => {
-    const errors = {};
+    let errors = {};
     if (!values.name) {
-      errors.name = "Required";
+      errors.name = "Username is required";
     }
     if (!values.password) {
-      errors.password = "Required";
+      errors.password = "Password is required";
     }
     return errors;
   };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setFormError(validate(formValue));
-    setIsSubmit(true);
-  };
+
   useEffect(() => {
-    if (Object.keys(formError).length === 0 && isSubmit) {
-      const dataForm = {
-        UserName: formValue.name,
-        Password: formValue.password,
-        Source: "D",
-        Device: "W",
-        NotToken: "",
-        AuthCode: "r1d3r",
-        Type: "POST",
-        FetchUrl: `https://testing.esnep.com/happyhomes/api/login`,
-      };
-      fetch(dataForm).then(function (result) {
-        if (result.StatusCode === 200) {
-          const postResult = result.Values[0];
+    if (Object.keys(formErrors).length === 0 && isSubmitting) {
+      fetch("https://testing.esnep.com/happyhomes/api/login", {
+        method: "POST",
+        body: JSON.stringify({
+          UserName: formValues.name,
+          Password: formValues.password,
+          Source: "D",
+          Device: "W",
+          NotToken: "",
+          IsAllow: "",
+          AuthCode: "r1d3r",
+          IsVerified: "Y",
+          Type: "POST",
+        }),
+        headers: {
+          "Content-Type": "application/json",
+          Signature: "p0m76",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.StatusCode === 200) {
+            const postResult = data.Values[0];
+            console.log(postResult);
 
-          setIsSubmit(false);
-          if (postResult.UserType === "S") {
-            localStorage.setItem("userInfo", JSON.stringify(postResult));
-            sessionStorage.setItem("userInfo", JSON.stringify(postResult));
-            login(postResult);
-            setIsSubmit(false);
-
-            navigate("/dashboard");
+            setIsSubmitting(false);
+            if (postResult.UserType === "S") {
+              localStorage.setItem("userInfo", JSON.stringify(postResult));
+              sessionStorage.setItem("userInfo", JSON.stringify(postResult));
+              login(postResult);
+              setIsSubmitting(false);
+              navigate("/dashboard");
+            } else {
+              toast.error("User not authorized");
+              setIsSubmitting(false);
+            }
+            // if (postResult.IsAllow === "N") {
+            //   localStorage.setItem("userInfo", JSON.stringify(postResult));
+            //   sessionStorage.setItem("userInfo", JSON.stringify(postResult));
+            //   login(postResult);
+            //   setIsSubmitting(false);
+            // } else {
+            //   toast.error("User not verified");
+            // }
           } else {
-            toast.error("user not authorized");
-            setIsSubmit(false);
+            setIsSubmitting(false);
+            toast.error("Invalid credentials");
           }
-        } else {
-          setIsSubmit(false);
-        }
-      });
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          setIsSubmitting(false);
+          toast.error("An error occurred. Please try again later.");
+        });
     }
-  }, [formError]);
+  }, [formErrors]);
 
   return (
     <div className="flex items-center justify-center h-screen">
@@ -85,16 +112,19 @@ const Login = () => {
             >
               Username
             </label>
-
             <input
               type="text"
               id="userName"
+              name="name"
               placeholder="Type your username"
               className="p-2 border rounded-md w-full"
-              // value={formValue.name}
+              value={formValues.name}
               onChange={handleChange}
               required
             />
+            {formErrors.name && (
+              <p className="text-red-500 text-sm">{formErrors.name}</p>
+            )}
           </div>
           <div className="mb-3">
             <label
@@ -106,12 +136,16 @@ const Login = () => {
             <input
               type="password"
               id="password"
+              name="password"
               placeholder="Type your password"
               className="p-2 border rounded-md w-full"
-              // value={formValue.password}
+              value={formValues.password}
               onChange={handleChange}
               required
             />
+            {formErrors.password && (
+              <p className="text-red-500 text-sm">{formErrors.password}</p>
+            )}
           </div>
           <p href="#" className="text-right text-sm  block mb-3">
             Forgot Password?
@@ -121,7 +155,7 @@ const Login = () => {
               type="submit"
               className="border-2 p-2 w-full text-white bg-gradient-to-r from-blue-500 to-pink-400 rounded-md hover:from-pink-500 hover:to-blue-500 focus:outline-none focus:border-blue-700 focus:ring focus:ring-blue-200"
             >
-              Login
+              {isSubmitting ? "Logging in..." : "Login"}
             </button>
           </div>
         </div>
