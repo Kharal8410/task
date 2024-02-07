35bc5d618a462ebf81a0c74debef60c94ebdf482
mukunda kharal <kharalmukunda2@gmail.com>
2024-02-07 11:47:00 +0545

    fix: routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import AuthContext from "./context/AuthContext";
 import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    localStorage.getItem("isLoggedIn") === "true"
+  );
+
+  useEffect(() => {
+    localStorage.setItem("isLoggedIn", isLoggedIn);
+  }, [isLoggedIn]);
 
   const login = () => {
     setIsLoggedIn(true);
   };
-
   const logout = () => {
     setIsLoggedIn(false);
   };
@@ -18,12 +23,7 @@ const App = () => {
   return (
     <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
       <Routes>
-        {isLoggedIn ? (
-          <Route Path="/dashboard" />
-        ) : (
-          <Route path="/" element={<Login />} />
-        )}
-
+        <Route path="/" element={<Login />} />
         <Route
           path="/dashboard"
           element={isLoggedIn ? <Dashboard /> : <Navigate to="/" />}
