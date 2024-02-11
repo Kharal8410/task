c71c43fa5a7f6584e653558fce9d70809ed80497
mukunda kharal <kharalmukunda2@gmail.com>
2024-02-11 10:48:04 +0545

    feat: changes on page not found

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 const PageNotFound = () => {
-  return <div className="p-2 m-2 text-lg font-bold ">Page Not Found...</div>;
+  return (
+    <>
+      <div className="flex flex-row gap-3 p-1 m-1">
+        <div>Page Not Found...</div>
+        <span>
+          {" "}
+          Click here to login
+          <a href="/" className="text-sm text-blue-500 underline ">
+            Login{" "}
+          </a>
+        </span>
+      </div>
+    </>
+  );
 };
 
 export default PageNotFound;
